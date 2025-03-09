75a7d8e7ebbea4e17579c92a241030c4f47a5869
Luis <losjuegosdelcapibara@gmail.com>
2025-03-09 09:42:00 -0600

    Challenge complete

--- app.js
+++ app.js
@@ -21,3 +21,51 @@ function calcularFactorial(numero) {
   let resultado = calcularFactorial(numero);
   console.log(`El factorial de ${numero} es ${resultado}`);
   
+function convertDivisa(dolar) {
+ let pesoMx = 20.26
+ conversion = pesoMx * dolar
+  console.log(conversion)
+}
+
+
+let dolares = 2
+convertDivisa(dolares);
+
+
+function medidas(altura, anchura) {
+
+  let area = altura * anchura
+  console.log(area)
+
+  let perimetro = (2 * altura) + (2 * anchura)
+  console.log(perimetro)
+  
+}
+
+medidas(2,4);
+
+
+
+function medidasCirculo(radio) {
+  let pi = 3.14
+
+  let area = pi * (radio * radio)
+  console.log(area)
+
+  let perimetro = (2 * pi) * radio
+  console.log(perimetro)
+}
+
+medidasCirculo(2);
+
+
+
+function tablaDelDos(num) {
+
+  let constant = 2
+  let resultado = constant * num
+  console.log(resultado);
+  
+}
+
+tablaDelDos(2);
